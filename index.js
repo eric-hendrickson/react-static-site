44f9918eafcedde5ce6ecaca518b4671a3a972fb
Eric Hendrickson <hendrickson02@gmail.com>
2018-07-01 00:16:17 -0700

    Can now build multiple routes

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
 import React from 'react'
-import { Link, StaticRouter as Router, Route } from 'react-router-dom'
+import { Link, BrowserRouter, StaticRouter, Route } from 'react-router-dom'
 import ReactDOMServer from 'react-dom/server'
-import Routes from './Routes.jsx'
+import ReactDOM from 'react-dom'
 
-const Home = () => (
+const Home = props => 
 	<div>
 	  <h2>Home</h2>
 	  <p>Hello, world</p>
 	</div>
-  );
 
-module.exports = function render(locals, callback) {
-    var html = ReactDOMServer.renderToStaticMarkup(
-		<Router>
-			<Route exact path='/' component={Home} />
-		</Router>
-    );
-    callback(null, html)
-}
+const Random = props =>
+	<div>
+		<h1>ANOTHER PATH!</h1>
+	</div>
 
+const Html = props => 
+	<html>
+		<head><title>My Static Site</title></head>
+		<body>
+		<div id="app">
+			{props.children}
+		</div>
+		<script src="/bundle.js"></script>
+		</body> 
+	</html>
+
+const routes = (
+	<div>
+		<Route exact path="/" component={Home} />
+		<Route exact path="/random" component={Random} />
+	</div>
+)
+
+export default locals => ReactDOMServer.renderToString(
+	<StaticRouter basename={locals.path} context={{}}>
+		<Html>
+			{routes}
+		</Html>
+	</StaticRouter>
+)
+
+if (typeof document != 'undefined') {
+	ReactDOM.render(
+	  <BrowserRouter>{routes}</BrowserRouter>,
+	  document.getElementById('app')
+	)
+  }
